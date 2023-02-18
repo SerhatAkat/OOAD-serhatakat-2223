4c10a5804ba10acf1e13d502df97d02cbc54ea45
Serhat Akat <serhat.akat@student.odisee.be>
2023-02-18 22:47:54 +0100

    update

---
+++ SlnLes03BestandenExcepties/WpfCopy/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
+
+
+namespace WpfCopy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string content = "";
+        string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        private void btnKies_Click(object sender, RoutedEventArgs e)
+        {
+            string chosenFileName = "";
+            btnGo.IsEnabled = true;
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            dialog.Filter = "Tekstbestanden|*.TXT;*.TEXT";
+
+            if (dialog.ShowDialog() == true)
+            {
+                chosenFileName = dialog.FileName;
+                txtFile.Text = chosenFileName;
+
+                string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                try
+                {
+                    string filePath = System.IO.Path.Combine(folderPath, chosenFileName);
+                    content = File.ReadAllText(filePath);
+                }
+                catch (FileNotFoundException ex)
+                {
+                    MessageBox.Show(
+                        $"{ex.FileName} kon niet gevonden worden", // boodschap
+                        "Oeps!", // titel
+                        MessageBoxButton.OK, // buttons
+                        MessageBoxImage.Error
+                    ); // error icoon
+                }
+
+            }
+            else
+            {
+                lblMsg.Content = "Kies een bestand";
+            }
+
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            dialog.Filter = "Tekstbestanden|*.TXT;*.TEXT";
+            dialog.FileName = "savedfile.txt";
+            if (dialog.ShowDialog() == true)
+            {
+                File.WriteAllText(dialog.FileName, content);
+                lblMsg.Content = "Bestand is overgezet";
+            }
+            else
+            {
+                lblMsg.Content = "Bestand is niet overgezet";
+            }
+
+        }
+    }
+}
---
+++ SlnLes03BestandenExcepties/WpfFileInfo/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+namespace WpfFileInfo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBestand_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Tekstbestanden|*.TXT;*.TEXT";
+
+            if (dialog.ShowDialog() == true)
+            {
+                string fileName = dialog.FileName;
+                string content = File.ReadAllText(fileName);
+                int teller = 0;
+
+                for (int i = 0; i < content.Length - 1; i++)
+                {
+                    if (content[i] == ' ' && Char.IsLetter(content[i + 1]) && (i > 0))
+                    {
+                        teller++;
+                    }
+                }
+
+                string[] lines = content.Split();
+                Dictionary<string, int> AantalWoorden = new Dictionary<string, int>();
+                foreach (string line in lines)
+                {
+                    if (line == "") continue;
+                    if (AantalWoorden.ContainsKey(line.ToLower()))
+                        AantalWoorden[line.ToLower()]++;
+                    else
+                        AantalWoorden[line.ToLower()] = 1;
+                }
+                AantalWoorden = AantalWoorden.OrderByDescending(a => a.Value).Take(3).ToDictionary(a => a.Key, x => x.Value);
+
+                FileInfo fi = new FileInfo(fileName);
+                lblNaam.Content = $"bestandsnaam: {fi.Name}";
+                lblExtensie.Content = $"extensie: {fi.Extension}";
+                lblDatum.Content = $"gemaakt op: {fi.CreationTime}";
+                lblMap.Content = $"mapnaam: {fi.Directory.Name}";
+                lblAantal.Content = $"aantal woorden: {teller}";
+                lblWoorden.Content = $"meest voorkomende woorden: {string.Join(", ", AantalWoorden.Keys)}";
+            }
+        }
+    }
+}
