0c6198ad38d33bd68417eb05352b6097f69191c9
Serhat Akat <serhat.akat@student.odisee.be>
2023-02-22 03:27:31 +0100

    update

---
+++ SlnLes03BestandenExcepties/ConsoleCsv/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCsv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            string[] Games = { "Schaken", "Backgammon", "Dammen" };
+            string[,] teams = new string[,] { { "Peter", "Jan", "Lukas", "Marie", "Kobe" }, { "Axel", "Pieter-Jan", "Serhat", "Niels", "Amber" } };
+            string Noteer = "";
+            for (int i = 0; i < 100; i++)
+            {
+                int game = rnd.Next(1, Games.Length);
+                int team = rnd.Next(0, 2);
+                int player = rnd.Next(0, 5);
+                int scndTeam;
+                int scndPlayer = rnd.Next(0, 5);
+                do
+                {
+                    scndTeam = rnd.Next(0, 2);
+                }
+                while (team != scndTeam);
+                Noteer += Convert.ToString(i + 1) + "; " + Convert.ToString(teams[team, player]) + ";" + Convert.ToString(teams[scndTeam, scndPlayer]) + ";" + Convert.ToString(Games[game]) + ";" + Convert.ToString(rnd.Next(0, 5)) + "-" + Convert.ToString(rnd.Next(0, 5)) + Environment.NewLine;
+            }
+            Console.WriteLine(Noteer);
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string filePath = System.IO.Path.Combine(folderPath, "wedstrijden.csv");
+            File.WriteAllText(filePath, Noteer);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SlnLes03BestandenExcepties/WpfCompare/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfCompare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PopulateFileList(lstFiles1);
+            PopulateFileList(lstFiles2);
+            lstFiles1.SelectionChanged += FileList_SelectionChanged;
+            lstFiles2.SelectionChanged += FileList_SelectionChanged;
+        }
+        private void PopulateFileList(ListBox listBox)
+        {
+            listBox.Items.Clear();
+            var files = Directory.GetFiles(folderPath, "*.txt");
+            foreach (var file in files)
+            {
+                listBox.Items.Add(Path.GetFileName(file));
+            }
+        }
+        private void FileList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var listBox = (ListBox)sender;
+            var fileName = (string)listBox.SelectedItem;
+            var filePath = Path.Combine(folderPath, fileName);
+            var fileContents = File.ReadAllText(filePath);
+
+            if (listBox == lstFiles1)
+            {
+                lstSummary1.Items.Clear();
+                foreach (var line in fileContents.Split('\n'))
+                {
+                    lstSummary1.Items.Add(line);
+
+                }
+            }
+            else if (listBox == lstFiles2)
+            {
+                lstSummary2.Items.Clear();
+                foreach (var line in fileContents.Split('\n'))
+                {
+                    lstSummary2.Items.Add(line);
+                }
+            }
+        }
+
+        private void btnCompare_Click(object sender, RoutedEventArgs e)
+        {
+            var differences = lstSummary1.Items.Cast<string>()
+                .Select((line, index) => new { Line = line, Index = index })
+                .Where(item => lstSummary2.Items.Count > item.Index && item.Line != lstSummary2.Items[item.Index] as string)
+                .Select(item => item.Index);
+
+            lstSummary2.SelectedItem = null;
+            foreach (var index in differences)
+            {
+                lstSummary2.SelectedItem = lstSummary2.Items[index];
+            }
+        }
+    }
+}
