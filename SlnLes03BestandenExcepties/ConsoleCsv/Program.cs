dfbb931c2eb79955c3c5bb6252b56f45d535c9e8
Serhat Akat <serhat.akat@student.odisee.be>
2023-02-28 23:54:36 +0100

    update

--- SlnLes03BestandenExcepties/ConsoleCsv/Program.cs
+++ SlnLes03BestandenExcepties/ConsoleCsv/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,21 +13,24 @@ namespace ConsoleCsv
         {
             Random rnd = new Random();
             string[] Games = { "Schaken", "Backgammon", "Dammen" };
-            string[,] teams = new string[,] { { "Zakaria", "Saleha", "Indra", "Ralph", "Francisco" }, { "Marie", "Jens", "Serhat", "Niels", "Amber" } };
+            string[] players = { "Zakaria", "Saleha", "Indra", "Ralph", "Francisco", "Marie", "Jens", "Serhat", "Niels", "Amber" };
             string Noteer = "";
             for (int i = 0; i < 100; i++)
             {
-                int game = rnd.Next(1, Games.Length);
-                int team = rnd.Next(0, 2);
-                int player = rnd.Next(0, 5);
-                int scndTeam;
-                int scndPlayer = rnd.Next(0, 5);
+                string player1, player2;
+                int game = rnd.Next(0, Games.Length);
+                int playerIndex1, playerIndex2;
                 do
                 {
-                    scndTeam = rnd.Next(0, 2);
+                    playerIndex1 = rnd.Next(0, players.Length);
+                    player1 = players[playerIndex1];
+                    playerIndex2 = rnd.Next(0, players.Length);
+                    player2 = players[playerIndex2];
                 }
-                while (team != scndTeam);
-                Noteer += Convert.ToString(i + 1) + "; " + Convert.ToString(teams[team, player]) + ";" + Convert.ToString(teams[scndTeam, scndPlayer]) + ";" + Convert.ToString(Games[game]) + ";" + Convert.ToString(rnd.Next(0, 5)) + "-" + Convert.ToString(rnd.Next(0, 5)) + Environment.NewLine;
+                while (player1 == player2); // spelers moeten verschillende namen hebben
+                int score1 = rnd.Next(0, 3);
+                int score2 = 3 - score1;
+                Noteer += Convert.ToString(i + 1) + "; " + player1 + ";" + player2 + ";" + Convert.ToString(Games[game]) + ";" + Convert.ToString(score1) + "-" + Convert.ToString(score2) + Environment.NewLine;
             }
             Console.WriteLine(Noteer);
             string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
