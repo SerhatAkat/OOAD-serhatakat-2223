0b8ae11f3efb136b0e35325adb0cbd236aa21af0
Serhat Akat <serhat.akat@student.odisee.be>
2023-03-04 23:57:41 +0100

    WpfFocusTab

---
+++ SlnLes04WpfLayout/WpfFocusTab/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfFocusTab
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox txtBox = (TextBox)sender;
+            txtBox.Background = Brushes.LimeGreen;
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox txtBox = (TextBox)sender;
+            txtBox.Background = Brushes.Transparent;
+        }
+    }
+}
