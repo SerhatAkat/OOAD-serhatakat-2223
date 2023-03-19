a6041fcf03713627cf5f89b90bf5b2e7730031e6
Serhat Akat <serhat.akat@student.odisee.be>
2023-03-19 03:00:09 +0100

    update

---
+++ SlnLes06ClassesProperties/ConsoleKaartspel1/Deck.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleKaartspel1
+{
+    internal class Deck
+    {
+        private List<Kaart> kaarten;
+
+        public List<Kaart> Kaarten
+        {
+            get { return kaarten; }
+        }
+
+        public Deck()
+        {
+            kaarten = new List<Kaart>();
+            for (int nummer = 1; nummer <= 13; nummer++)
+            {
+                kaarten.Add(new Kaart(nummer, "C"));
+                kaarten.Add(new Kaart(nummer, "S"));
+                kaarten.Add(new Kaart(nummer, "H"));
+                kaarten.Add(new Kaart(nummer, "D"));
+            }
+        }
+
+        public void Schudden()
+        {
+            Random random = new Random();
+            int n = kaarten.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                Kaart kaart = kaarten[k];
+                kaarten[k] = kaarten[n];
+                kaarten[n] = kaart;
+            }
+        }
+
+        public Kaart NeemKaart()
+        {
+            if (kaarten.Count == 0)
+            {
+                throw new InvalidOperationException("Geen kaarten meer in het deck.");
+            }
+            Kaart kaart = kaarten[0];
+            kaarten.RemoveAt(0);
+            return kaart;
+        }
+    }
+}
---
+++ SlnLes06ClassesProperties/ConsoleKaartspel1/Kaart.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleKaartspel1
+{
+    internal class Kaart
+    {
+        private int nummer;
+        private string kleur;
+
+        public int Nummer
+        {
+            get { return nummer; }
+            set
+            {
+                if (value < 1 || value > 13)
+                {
+                    throw new ArgumentException("Nummer moet tussen 1 en 13 zijn.");
+                }
+                nummer = value;
+            }
+        }
+
+        public string Kleur
+        {
+            get { return kleur; }
+            set
+            {
+                if (value != "C" && value != "S" && value != "H" && value != "D")
+                {
+                    throw new ArgumentException("Kleur moet C, S, H of D zijn.");
+                }
+                kleur = value;
+            }
+        }
+
+        public Kaart(int nummer, string kleur)
+        {
+            Nummer = nummer;
+            Kleur = kleur;
+        }
+    }
+}
