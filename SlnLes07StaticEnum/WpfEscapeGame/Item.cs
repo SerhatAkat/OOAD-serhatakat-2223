b4adb66c9f2d3fc554d214b2a6ad41c49bbc578b
Serhat Akat <serhat.akat@student.odisee.be>
2023-04-25 17:18:25 +0200

    update

---
+++ SlnLes07StaticEnum/WpfEscapeGame/Item.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfEscapeGame
+{
+    internal class Item
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsLocked { get; set; } = false;
+        public bool IsPortable { get; set; } = true;
+        public Item Key { get; set; }
+        public Item HiddenItem { get; set; }
+
+        public Item(string name, string desc)
+        {
+            Name = name;
+            Description = desc;
+        }
+
+        public Item(string name, string desc, bool isPortable)
+        {
+            Name = name;
+            Description = desc;
+            IsPortable = isPortable;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
