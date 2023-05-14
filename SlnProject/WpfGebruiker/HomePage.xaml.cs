eb29fbdd303a9edc741f2a65770475ac5c2ee855
Serhat Akat <serhat.akat@student.odisee.be>
2023-05-14 12:35:52 +0200

    update

--- SlnProject/WpfGebruiker/HomePage.xaml.cs
+++ SlnProject/WpfGebruiker/HomePage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MyClassLibrary;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,68 @@ namespace WpfGebruiker
         public HomePage()
         {
             InitializeComponent();
+
+            // Haal de lijst met voertuigen op
+            List<Voertuig> voertuigen = Voertuig.GetAllVoertuigen();
+            foreach (var voertuig in voertuigen)
+            {
+                // Haal de bijbehorende foto op
+                Foto foto = Foto.GetFotoForVoertuig(voertuig.Id);
+
+                // Aangenomen dat foto.Image een byte array is
+                BitmapImage bitmap = new BitmapImage();
+                using (var mem = new MemoryStream(foto.Image))
+                {
+                    mem.Position = 0;
+                    bitmap.BeginInit();
+                    bitmap.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                    bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmap.UriSource = null;
+                    bitmap.StreamSource = mem;
+                    bitmap.EndInit();
+                }
+                bitmap.Freeze();
+
+                Image img = new Image();
+                img.Width = 80;
+                img.Source = bitmap;
+
+                Label lblNaam = new Label();
+                lblNaam.Content = voertuig.Naam;
+                lblNaam.FontWeight = FontWeights.Bold;
+
+                Label lblMerk = new Label();
+                lblMerk.Content = voertuig.Merk;
+
+                Label lblModel = new Label();
+                lblModel.Content = voertuig.Model;
+
+                Button btn = new Button();
+                btn.Tag = voertuig.Id;
+                btn.Content = "Info";
+                btn.Click += VoertuigChosen;
+
+                // Maak een StackPanel om de labels en de knop te groeperen
+                StackPanel pnlLabels = new StackPanel();
+                pnlLabels.Children.Add(lblNaam);
+                pnlLabels.Children.Add(lblMerk);
+                pnlLabels.Children.Add(lblModel);
+                pnlLabels.Children.Add(btn);
+
+                // Maak een WrapPanel om de afbeelding en de StackPanel te groeperen
+                WrapPanel pnl = new WrapPanel();
+                pnl.Margin = new Thickness(0, 0, 20, 20);
+                pnl.Children.Add(img);
+                pnl.Children.Add(pnlLabels);
+
+                // Voeg de WrapPanel toe aan het hoofdpaneel
+                pnlItems.Children.Add(pnl);
+            }
+        }
+        private void VoertuigChosen(object sender, RoutedEventArgs e)
+        {
+            Button clicked = sender as Button;
+            MessageBox.Show($"Voertuig {clicked.Tag} geselecteerd");
         }
         private void GoBack_Click(object sender, RoutedEventArgs e)
         {
--- SlnProject/WpfGebruiker/MainWindow.xaml.cs
+++ SlnProject/WpfGebruiker/MainWindow.xaml.cs
@@ -19,71 +19,21 @@ namespace WpfGebruiker
         {
             InitializeComponent();
             ingelogdeGebruiker = gebruiker;
-
-            // Haal de lijst met voertuigen op
-            List<Voertuig> voertuigen = Voertuig.GetAllVoertuigen();
-            foreach (var voertuig in voertuigen)
-            {
-                // Haal de bijbehorende foto op
-                Foto foto = Foto.GetFotoForVoertuig(voertuig.Id);
-
-                // Aangenomen dat foto.Image een byte array is
-                BitmapImage bitmap = new BitmapImage();
-                using (var mem = new MemoryStream(foto.Image))
-                {
-                    mem.Position = 0;
-                    bitmap.BeginInit();
-                    bitmap.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
-                    bitmap.CacheOption = BitmapCacheOption.OnLoad;
-                    bitmap.UriSource = null;
-                    bitmap.StreamSource = mem;
-                    bitmap.EndInit();
-                }
-                bitmap.Freeze();
-
-                Image img = new Image();
-                img.Width = 80;
-                img.Source = bitmap;
-
-                Label lbl = new Label();
-                lbl.Content = $"{voertuig.Merk} {voertuig.Model}";
-
-                Button btn = new Button();
-                btn.Tag = voertuig.Id;
-                btn.Content = "Info";
-                btn.Click += VoertuigChosen;
-
-                // Maak een StackPanel om de afbeelding, het label en de knop te groeperen
-                StackPanel pnl = new StackPanel();
-                pnl.Margin = new Thickness(0, 0, 20, 20);
-                pnl.Children.Add(img);
-                pnl.Children.Add(lbl);
-                pnl.Children.Add(btn);
-
-                // Voeg de StackPanel toe aan het hoofdpaneel
-                pnlItems.Children.Add(pnl);
-            }
         }
 
         private void btnHome_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Navigate(new HomePage());
+            MainFrame.Content = new HomePage();
         }
 
         private void btnOntleningen_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Navigate(new OntleningenPage());
+            MainFrame.Content = new OntleningenPage();
         }
 
         private void btnVoertuigen_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Navigate(new VoertuigenPage());
+            MainFrame.Content = new VoertuigenPage();
         }
-        private void VoertuigChosen(object sender, RoutedEventArgs e)
-        {
-            Button clicked = sender as Button;
-            MessageBox.Show($"Voertuig {clicked.Tag} geselecteerd");
-        }
-
     }
 }
